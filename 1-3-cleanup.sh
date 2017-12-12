3669f4518e4cd081cf4595dbb09abd71dab4904b
amarchahal98 <amarchahal98@gmail.com>
2017-12-12 14:42:26 -0800

    finish 1-3

--- 1-3-cleanup.sh
+++ 1-3-cleanup.sh
@@ -4,7 +4,7 @@ logdir=/var/log
 root_uid=0
 id_notroot=3
 LINES=50
-
+nocd=4
 
 # Root Check
 if [[ $EUID -ne $root_uid ]];
@@ -19,4 +19,21 @@ then
   lines=$1
 else
   lines=$LINES # Default if no argument provided
+fi
 
+
+cd $logdir
+
+if [[ `pwd` != $logdir ]];
+then
+  echo "Could not change directories to $logdir."
+  exit $nocd
+fi
+
+tail -n $lines messages > mesg.tmp
+mv mesg.tmp messages
+
+cat /dev/null > otherlogs
+echo "Log files have been cleaned."
+
+exit 0
